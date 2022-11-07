5a074b6659c4cce93a151ac53a4b2bb36ab4932a
Matija Stepanic <stepanic.matija@gmail.com>
2022-11-07 14:10:36 +0100

    Fixed installer

--- installer.sh
+++ installer.sh
@@ -5,5 +5,5 @@ curl -L -k https://github.com/stepanic/flutterflow-socket/archive/refs/heads/mai
 chmod +x ./run ./watch ./tools/sync ./tools/hotreloader.sh ./tools/github/listen
 
 git add ./run ./watch ./tools && npm install --prefix tools/github/
-commit -m "Installed flutterflow-socket"
+git commit -m "Installed flutterflow-socket"
 git push
