627752b3a0e0e43ff485c05ee1bb44a1c8ed6c04
Matija Stepanic <stepanic.matija@gmail.com>
2023-09-25 14:30:56 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
+if which node > /dev/null
+then
+    echo "node is installed ✅"
+else
+    echo "node is NOT installed ❌"
+    echo "check: https://github.com/nvm-sh/nvm"
+    exit 1
+fi
+
+if which npm > /dev/null
+then
+    echo "npm is installed ✅"
+else
+    echo "npm is NOT installed ❌"
+    echo "check: https://github.com/nvm-sh/nvm"
+    exit 1
+fi
+
 curl -L -k https://github.com/stepanic/flutterflow-socket/archive/refs/heads/main.zip | tar -xz --strip-components=1 flutterflow-socket-main/run flutterflow-socket-main/watch flutterflow-socket-main/tools
 
 chmod +x ./run ./watch ./tools/sync ./tools/hotreloader.sh ./tools/github/listen
 
-git add ./run ./watch ./tools && npm install --prefix tools/github/
+git add ./run ./watch ./tools && cd ./tools/github && npm install && cd ../..
 git commit -m "Installed flutterflow-socket"
 git push
