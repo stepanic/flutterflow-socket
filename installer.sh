98e1681533aecfde645cb82105b92346f68348ff
Matija Stepanic <stepanic.matija@gmail.com>
2023-09-25 14:37:26 +0200

    Edited installer

--- installer.sh
+++ installer.sh
@@ -21,7 +21,8 @@ fi
 curl -L -k https://github.com/stepanic/flutterflow-socket/archive/refs/heads/main.zip | tar -xz --strip-components=1 flutterflow-socket-main/run flutterflow-socket-main/watch flutterflow-socket-main/tools
 
 chmod +x ./run ./watch ./tools/sync ./tools/hotreloader.sh ./tools/github/listen
+cd ./tools/github && npm install && cd ../..
 
-git add ./run ./watch ./tools && cd ./tools/github && npm install && cd ../..
+git add ./run ./watch ./tools
 git commit -m "Installed flutterflow-socket"
 git push
